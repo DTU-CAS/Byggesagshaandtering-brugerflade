a91e33e7cdfeafc08b4b8b5a37abcbd1d718bcf9
casperfibaek <casperfibaek@gmail.com>
2016-11-04 15:46:07 +0100

    css fixes

--- js/custom/infoPanel.js
+++ js/custom/infoPanel.js
@@ -2,8 +2,14 @@ function infoPanel(obj){
   var table = "<table>";
   var keys = Object.keys(obj);
   for (var i = 0; i < keys.length; i++) {
-      table += "<tr><td>" + keys[i] + "</td>" + "<td>" +  obj[keys[i]] + "</td></tr>";
-      console.log(keys[i] + ": " + obj[keys[i]]);
+      table += "<tr><td>" + keys[i] + "</td>";
+      if(typeof(obj[keys[i]]) === "number"){
+        table += "<td class='number'>" +  obj[keys[i]] + "</td></tr>";
+      } else if(obj[keys[i]] === "boolean" || obj[keys[i]] === "true" || obj[keys[i]] === "false"){
+        table += "<td class='boolean'>" +  obj[keys[i]] + "</td></tr>";
+      } else {
+        table += "<td>" +  obj[keys[i]] + "</td></tr>";
+      }
   }
   return table;
 }
