e957df7b60b7bce4a8e863d32a4aa5bd52792ecd
casperfibaek <casperfibaek@gmail.com>
2016-11-04 14:06:49 +0100

    testData

--- js/custom/init.js
+++ js/custom/init.js
@@ -3,16 +3,28 @@
 function init(){
   // Query the URL for parameters
   var query = QueryString();
-  console.log(query);
+  if(query){jQuery("#interface").append("<h4>" + "Byggesag: " + query.ID + "</h4>");}
+
+  // Start loading geometry and attributes from MSSQL server with ID
+
+  // placeholder loader
+  function spin(){
+    jQuery("body").append("<div class='spinner'></div>");
+    setTimeout(endLoad, 3000);
+    function endLoad(){
+      jQuery(".spinner").remove();
+    }
+  }
+  spin();
 
   // create the map
-    map = L.map('map', {
-      center: [55.783014, 12.519372],
-      zoom: 17,
-      maxZoom: 21,
-      minZoom: 14,
-      zoomControl: false
-    });
+  map = L.map('map', {
+    center: [55.783014, 12.519372],
+    zoom: 17,
+    maxZoom: 21,
+    minZoom: 14,
+    zoomControl: false
+  });
 
   // GST Ortho 2016
   var	GST_Ortho = L.tileLayer.wms('http://kortforsyningen.kms.dk/?servicename=orto_foraar', {
@@ -41,5 +53,7 @@ function init(){
   };
 
   L.control.layers(basemaps, {}, {collapsed: false}).addTo(map);
+
+
 
 }
