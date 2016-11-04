2de82d6ad234f21d88ad03c1824db9a9de6c0612
casperfibaek <casperfibaek@gmail.com>
2016-11-04 13:10:34 +0100

    generic statement

--- js/custom/init.js
+++ js/custom/init.js
@@ -1,7 +1,45 @@
-function init(){
+// Initialize the interface
 
+function init(){
   // Query the URL for parameters
   var query = QueryString();
   console.log(query);
+
+  // create the map
+    map = L.map('map', {
+      center: [55.783014, 12.519372],
+      zoom: 17,
+      maxZoom: 21,
+      minZoom: 14,
+      zoomControl: false
+    });
+
+  // GST Ortho 2016
+  var	GST_Ortho = L.tileLayer.wms('http://kortforsyningen.kms.dk/?servicename=orto_foraar', {
+    login: 'qgisdk',
+    password: 'qgisdk',
+    version: '1.1.1',
+    layers: 'orto_foraar',
+    format: 'image/png',
+    continuousWorld : true,
+    maxZoom: 21,
+    maxNativeZoom: 18,
+    attribution: '&copy; <a href="http://gst.dk">GeoDanmark</a>'
+  }).addTo(map);
+
+  var OSMbasemap = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors,' +
+          '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+    maxZoom: 21,
+    maxNativeZoom: 18
+  });
+
+  // Add to layer control
+  var basemaps = {
+    "GST": GST_Ortho,
+    "OSM": OSMbasemap
+  };
+
+  L.control.layers(basemaps, {}, {collapsed: false}).addTo(map);
 
 }
